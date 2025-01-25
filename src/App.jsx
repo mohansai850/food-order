8af58ff17ab66b336e1b8b7d6e53dff6fbe8ba7b
Mohan Sai <mohansai850@gmail.com>
2025-01-25 11:50:40 +0530

    eslint setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import React from 'react';
+
 function App() {
   return (
     <>
       <h1>You got this 💪</h1>
       <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p>
+      <p>Don&apos;t worry - we&apos;ve all been there. Let&apos;s build it together!</p>
     </>
   );
 }
